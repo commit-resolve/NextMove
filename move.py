d55abd4eb2a83f354f8ca6dbee23c05cb12c0a22
hema <mystartup.docs@gmail.com>
2026-03-01 19:36:39 +0530

    integrated with stockfish

---
+++ move.py
@@ -0,0 +1,37 @@
+import chess
+import chess.engine
+
+# Create board
+board = chess.Board()
+
+# Connect to Stockfish
+engine = chess.engine.SimpleEngine.popen_uci("../stockfish/stockfish-macos-m1-apple-silicon")
+print(engine.id)
+print(board)
+
+while not board.is_game_over():
+
+    user_move = input("Your move (e2e4): ")
+
+    try:
+        move = chess.Move.from_uci(user_move)
+
+        if move in board.legal_moves:
+            board.push(move)
+            print("\nYou played:", user_move)
+            print(board)
+
+            # Engine move
+            result = engine.play(board, chess.engine.Limit(time=0.5))
+            board.push(result.move)
+
+            print("\nStockfish played:", result.move)
+            print(board)
+
+        else:
+            print("Illegal move!")
+
+    except:
+        print("Invalid format!")
+
+engine.quit()
